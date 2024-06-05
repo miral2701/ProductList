7ac34d1a63de597fe2f55ca81ba3ef4234b577d9
Mira Madani <miramadani2701@gmail.com>
2024-06-05 14:59:34 +0300

    Add project files.

---
+++ ProductList/AddProduct.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProductList
+{
+    /// <summary>
+    /// Interaction logic for AddProduct.xaml
+    /// </summary>
+    public partial class AddProduct : Window
+    {
+        TextBlock t1;
+        TextBlock t2;
+        public AddProduct(TextBlock t1,TextBlock t2)
+        {
+            InitializeComponent();
+            this.t1 = t1;
+            this.t2 = t2;   
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            t1.Text=textBox1.Text; 
+            t2.Text=textBox2.Text;
+            if (t1.Text == "" && t2.Text == "")
+            {
+                MessageBox.Show("Empty string");
+            }
+            else
+            {
+                this.Close();
+
+            }
+        }
+    }
+}
---
+++ ProductList/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ListBox a=new ListBox();
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            stackPanel.Children.Add(a);
+
+
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlock a1 = new TextBlock();
+            TextBlock a2 = new TextBlock();
+            AddProduct p = new AddProduct(a1,a2);
+          
+            p.Show();
+            
+          
+
+            Expander exp = new Expander();
+
+            //Create Bullet Panel for Expander Header
+            BulletDecorator bp = new BulletDecorator();
+            
+            a1.Margin = new Thickness(20, 0, 0, 0);
+            bp.Child = a1;
+            exp.Header = bp;
+
+            //Create TextBlock with ScrollViewer for Expander Content
+            StackPanel spScroll = new StackPanel();
+            
+            a2.TextWrapping = TextWrapping.Wrap;
+
+            spScroll.Children.Add(a2);
+            ScrollViewer scr = new ScrollViewer();
+            scr.Content = spScroll;
+            scr.Height = 50;
+            exp.Content = scr;
+
+            exp.Width = 200;
+            exp.HorizontalContentAlignment = HorizontalAlignment.Stretch;
+            //Insert Expander into the StackPanel and add it to the
+            //Grid
+           
+            a.Items.Add(exp);
+          
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            a.Items.Remove(a.SelectedItem);
+        }
+    }
+}
